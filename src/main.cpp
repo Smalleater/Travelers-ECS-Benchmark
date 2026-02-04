9458cc6105b9aded26a82e6b2cbbb1d4fac50452
Adrien Claude <163728051+Smalleater@users.noreply.github.com>
2026-02-04 11:03:14 +0100

    Finished adding benchmark

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,20 @@
 #include "TRA/ecs/world.hpp"
 #include "TRA/ecs/component.hpp"
 #include "TRA/ecs/tag.hpp"
+#include "TRA/ecs/ISystem.hpp"
 
 constexpr size_t LOOP_COUNT = 10;
-constexpr uint32_t ENTITY_COUNT = 1000000;
+constexpr uint32_t ENTITY_COUNT = 100000;
 constexpr size_t COMPONENTS_PER_ENTITY = 5;
+constexpr size_t TAG_PER_ENTITY = 3;
+constexpr size_t SYSTEM_COUNT = 3;
+constexpr size_t UPDATE_SYSTEM_COUNT = 4;
 
 using namespace tra;
 
 TRA_ECS_REGISTER_TAG(IsAliveTag)
+TRA_ECS_REGISTER_TAG(Tag0)
+TRA_ECS_REGISTER_TAG(Tag1)
 
 TRA_ECS_REGISTER_COMPONENT(PositionComponent,
 	float m_x = 0;
@@ -20,24 +26,72 @@ TRA_ECS_REGISTER_COMPONENT(PositionComponent,
 )
 
 TRA_ECS_REGISTER_COMPONENT(VelocityComponent,
-	float m_x = 0;
-	float m_y = 0;
+	float m_x = 1;
+	float m_y = 1;
 )
 
 TRA_ECS_REGISTER_COMPONENT(DataComponent,
 	std::string m_name;
 	int m_gold = 9999;
 
-	DataComponent(const std::string& _name) : m_name(_name) {}
+DataComponent(const std::string& _name) : m_name(_name) {}
 )
 
 TRA_ECS_REGISTER_COMPONENT(HealthComponent,
-	int m_health = 100;
+	int m_health = 50;
 )
 
 TRA_ECS_REGISTER_COMPONENT(DamageComponent,
 	int m_damage = 0;
 )
+
+struct MoveSystem : ecs::ISystem
+{
+	void update(ecs::World* _world) override
+	{
+		for (auto& [entity, positionPtr, velocityPtr] : _world->queryEntities(
+			ecs::WithComponent<ecs::PositionComponent, ecs::VelocityComponent>{},
+			ecs::WithoutComponent<>{},
+			ecs::WithTag<ecs::IsAliveTag>{}))
+		{
+			positionPtr->m_x += velocityPtr->m_x;
+			positionPtr->m_y += velocityPtr->m_y;
+		}
+	}
+};
+
+struct DamageSystem : ecs::ISystem
+{
+	void update(ecs::World* _world) override
+	{
+		for (auto& [entity, damagePtr] : _world->queryEntities(
+			ecs::WithComponent<ecs::DamageComponent>{},
+			ecs::WithoutComponent<>{},
+			ecs::WithTag<ecs::IsAliveTag>{}))
+		{
+			damagePtr->m_damage = 25;
+		}
+	}
+};
+
+struct HealthSystem : ecs::ISystem
+{
+	void update(ecs::World* _world) override
+	{
+		for (auto& [entity, healthPtr, damagePtr] : _world->queryEntities(
+			ecs::WithComponent<ecs::HealthComponent, ecs::DamageComponent>{},
+			ecs::WithoutComponent<>{},
+			ecs::WithTag<ecs::IsAliveTag>{}))
+		{
+			healthPtr->m_health -= damagePtr->m_damage;
+			if (healthPtr->m_health <= 0)
+			{
+				healthPtr->m_health = 0;
+				_world->removeTag<ecs::IsAliveTag>(entity);
+			}
+		}
+	}
+};
 
 int main()
 {
@@ -46,19 +100,31 @@ int main()
 	std::chrono::time_point end = mainClock.now();
 
 	long long createEntityDuration = 0;
+	long long destroyEntityDuration = 0;
+
+	long long addTagDuration = 0;
+	long long removeTagDuration = 0;
+
 	long long addComponentDuration = 0;
+	long long removeComponentDuration = 0;
+
 	long long getComponentDuration = 0;
 	long long setComponentDuration = 0;
-	long long removeComponentDuration = 0;
-	long long destroyEntityDuration = 0;
+
+	long long systemDuration = 0;
+	
+	
 
 	std::vector<ecs::Entity> entities;
 	entities.resize(ENTITY_COUNT, ecs::NULL_ENTITY);
 
-
 	for (size_t i = 0; i < LOOP_COUNT; i++)
 	{
 		ecs::World ecsWorld;
+
+		ecsWorld.addSystem(std::make_unique<MoveSystem>());
+		ecsWorld.addSystem(std::make_unique<DamageSystem>());
+		ecsWorld.addSystem(std::make_unique<HealthSystem>());
 
 		//////////////////////////////////////////////////////////////
 
@@ -89,6 +155,21 @@ int main()
 		end = mainClock.now();
 
 		addComponentDuration += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
+		//////////////////////////////////////////////////////////////
+
+		start = mainClock.now();
+
+		for (size_t i = 0; i < ENTITY_COUNT; i++)
+		{
+			ecsWorld.addTag<ecs::IsAliveTag>(entities[i]);
+			ecsWorld.addTag<ecs::Tag0>(entities[i]);
+			ecsWorld.addTag<ecs::Tag1>(entities[i]);
+		}
+
+		end = mainClock.now();
+
+		addTagDuration += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
 
 		//////////////////////////////////////////////////////////////
 
@@ -128,6 +209,33 @@ int main()
 
 		start = mainClock.now();
 
+		for (size_t i = 0; i < UPDATE_SYSTEM_COUNT; i++)
+		{
+			ecsWorld.updateSystems();
+		}
+
+		end = mainClock.now();
+
+		systemDuration += std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+
+		//////////////////////////////////////////////////////////////
+
+		start = mainClock.now();
+
+		for (size_t i = 0; i < ENTITY_COUNT; i++)
+		{
+			ecsWorld.removeTag<ecs::Tag0>(entities[i]);
+			ecsWorld.removeTag<ecs::Tag1>(entities[i]);
+		}
+
+		end = mainClock.now();
+
+		removeTagDuration += std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+
+		//////////////////////////////////////////////////////////////
+
+		start = mainClock.now();
+
 		for (size_t i = 0; i < ENTITY_COUNT; i++)
 		{
 			ecsWorld.removeComponent<ecs::DamageComponent>(entities[i]);
@@ -161,15 +269,21 @@ int main()
 	std::cout << std::endl;
 	std::cout << "Average time to create an entity: " << std::to_string(createEntityDuration / LOOP_COUNT / ENTITY_COUNT) << " nanoseconds" << std::endl;
 	std::cout << std::endl;
+	std::cout << "Average time to destroy an entity: " << std::to_string(destroyEntityDuration / LOOP_COUNT / ENTITY_COUNT) << " nanoseconds" << std::endl;
+	std::cout << std::endl;
+	std::cout << "Average time to add a tag to an entity: " << std::to_string(addTagDuration / LOOP_COUNT / TAG_PER_ENTITY / ENTITY_COUNT) << " nanoseconds" << std::endl;
+	std::cout << std::endl;
+	std::cout << "Average time to remove a tag to an entity: " << std::to_string(removeTagDuration / LOOP_COUNT / (TAG_PER_ENTITY - 1) / ENTITY_COUNT) << " nanoseconds" << std::endl;
+	std::cout << std::endl;
 	std::cout << "Average time to add a component to an entity: " << std::to_string(addComponentDuration / LOOP_COUNT / COMPONENTS_PER_ENTITY / ENTITY_COUNT) << " nanoseconds" << std::endl;
+	std::cout << std::endl;
+	std::cout << "Average time to remove a component to an entity: " << std::to_string(removeComponentDuration / LOOP_COUNT / COMPONENTS_PER_ENTITY / ENTITY_COUNT) << " nanoseconds" << std::endl;
 	std::cout << std::endl;
 	std::cout << "Average time to get a component to an entity: " << std::to_string(getComponentDuration / LOOP_COUNT / COMPONENTS_PER_ENTITY / ENTITY_COUNT) << " nanoseconds" << std::endl;
 	std::cout << std::endl;
 	std::cout << "Average time to set a component to an entity: " << std::to_string(setComponentDuration / LOOP_COUNT / COMPONENTS_PER_ENTITY / ENTITY_COUNT) << " nanoseconds" << std::endl;
 	std::cout << std::endl;
-	std::cout << "Average time to remove a component to an entity: " << std::to_string(removeComponentDuration / LOOP_COUNT / COMPONENTS_PER_ENTITY / ENTITY_COUNT) << " nanoseconds" << std::endl;
-	std::cout << std::endl;
-	std::cout << "Average time to destroy an entity: " << std::to_string(destroyEntityDuration / LOOP_COUNT / ENTITY_COUNT) << " nanoseconds" << std::endl;
+	std::cout << "Average time to update a system to an entity: " << std::to_string(systemDuration / LOOP_COUNT / UPDATE_SYSTEM_COUNT / SYSTEM_COUNT) << " microseconds" << std::endl;
 
 	return 0;
 }
